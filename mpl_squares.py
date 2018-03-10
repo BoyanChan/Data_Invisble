76db8d9d21c52c924637d85a6da64b24d1f5d4a3
Kaser <chankaser@gmail.com>
2018-03-10 12:30:00 +0800

    2018.3.10

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,1 +1,17 @@
 import matplotlib.pyplot as plt
+
+x_values = list(range(1,5001))
+y_values = [x**3 for x in x_values]
+
+plt.scatter(x_values, y_values, c=y_values,cmap=plt.cm.Greens,edgecolors='none',s=10)
+#1.x的值
+#2.y的值
+#3.4.将y值转成渐变色,值越大,颜色越深
+#5.设定刻度显示
+#6.设定每个点的大小
+plt.title("Square Numbers", fontsize=20)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+plt.savefig('square.png',bbox_inches='tight')#用于将文件保存到文件目录下
+plt.show()
